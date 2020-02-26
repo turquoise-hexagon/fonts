f89db1f7edf899c1ef0ca5b070b6281f5da1e652
turquoise-hexagon <camille@lzr.pw>
2020-02-26 15:34:08 +0100

    make readme a bit more user friendly

--- make.sh
+++ make.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-# ensure fonttosfnt is installed
-if ! type fonttosfnt > /dev/null 2>&1; then
+type fonttosfnt > /dev/null 2>&1 || {
     echo 'error : install fonttosfnt first' >&2
     exit 1
-fi
+}
 
-for font in *.bdf; do
-    fonttosfnt -v -m 2 -o "${font%.*}.otb" "$font"
+rm -rf   bin
+mkdir -p bin
+
+for src in src/*.bdf; do
+    bin=${src##*/}
+    bin=bin/${bin%.bdf}.otb
+
+    fonttosfnt -o "$bin" "$src"
 done
